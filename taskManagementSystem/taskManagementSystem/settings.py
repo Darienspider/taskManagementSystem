a841a58949cf743bef7fcf22873a899b8faf1008
Shadarien Williams <56908337+Darienspider@users.noreply.github.com>
2024-11-21 13:20:41 -0500

    Update settings.py

--- taskManagementSystem/taskManagementSystem/settings.py
+++ taskManagementSystem/taskManagementSystem/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shaypythonsender@gmail.com'
-EMAIL_HOST_PASSWORD = 'mlwq gwtc otgg zzph'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = '../home/'
